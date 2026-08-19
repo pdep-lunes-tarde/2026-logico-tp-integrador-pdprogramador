813203bb362264b41c54aec31e7ed81c789abf88
NehuenUTN <nbergaminpaszkaniak@frba.utn.edu.ar>
2026-08-18 21:59:41 -0300

    Correcciones Parte 1 + Punto 5

--- src/tp.pl
+++ src/tp.pl
@@ -1,5 +1,5 @@
-% PARTE N°1
-habitante(denken, humano,1290, auberst).
+% PARTE N°1 (Punto 1)
+habitante(denken, humano, 1290, auberst).
 habitante(voll, enano, 1200, ende).
 habitante(serie, elfo, 500, weise).
 habitante(fern, humano, 1370, weise).
@@ -11,80 +11,83 @@ habitante(lernen, humano, 1315, auberst).
 habitante(frieren, elfo, 100, weise).
 habitante(eisen, enano, 1150, riegel).
 
-promedioVida(humano,80).
-promedioVida(enano,350).
+promedioVida(humano, 80).
+promedioVida(enano, 350).
+
+% Agregado para tener un generador de pueblos unicos y evitar repetir los calculos por cada persona que vivia en un pueblo.
+pueblo(Pueblo) :-
+    findall(P, habitante(_, _, _, P), ListaPueblos),
+    list_to_set(ListaPueblos, PueblosUnicos),
+    member(Pueblo, PueblosUnicos).
 
 anioDeMuerte(Persona, AnioMuerte) :- 
-    habitante(Persona, Especie, Nacimiento,_),
+    habitante(Persona, Especie, Nacimiento, _),
     promedioVida(Especie, Promedio),
     AnioMuerte is Nacimiento + Promedio.
 
 estaViva(Persona, Anio) :-
-    habitante(Persona,elfo,Nacimiento,_),
-    Anio >= Nacimiento.
-
-estaViva(Persona, Anio) :-
-    habitante(Persona,_,Nacimiento,_),
+    habitante(Persona, _, Nacimiento, _),
     Anio >= Nacimiento,
-    anioDeMuerte(Persona, AnioMuerte),
-    AnioMuerte >= Anio.
+    not(yaFallecio(Persona, Anio)).
 
-% PARTE N°2
+% correcion al punto 1b
+yaFallecio(Persona, Anio) :-
+    anioDeMuerte(Persona, AnioMuerte),
+    Anio > AnioMuerte.
+
+
+% PARTE N°1 (Puntos 2 y 3)
+
 recuerdo(wirbel, rescatar_a_la_hermana, presencio, 1390).
 recuerdo(frieren, rescatar_a_la_hermana , presencio, 1390).
 recuerdo(lawine, destruir_al_demonio_aura, cancion, 1393).
 recuerdo(voll, destruir_al_demonio_aura, libro(50), 1400).
 recuerdo(serie, destruir_al_rey_demonio, libro(100), 1335).
 recuerdo(kanne, recuperar_al_gato_perdido, presencio, 1375).
+recuerdo(denken, rescatar_a_la_hermana, libro(50), 1400). % Agregado para test de Punto 5
 
-hazania(rescatar_a_la_hermana, klares, dos(stark, fern)).
-hazania(destruir_al_demonio_aura, weise, frieren).
-hazania(destruir_al_demonio_aura, auberst, denken).
-hazania(destruir_al_rey_demonio, ende, cuatro(frieren, himmel, heiter, eisen)).
-hazania(recuperar_al_gato_perdido, weise, dos(himmel, frieren)).
-hazania(destruir_a_schlat_el_omnisciente, ende, el_heroe_del_sur).
+% implementacion de listas en vez de functores dos/2 o cuatro/4
+hazania(rescatar_a_la_hermana, klares, [stark, fern]).
+hazania(destruir_al_demonio_aura, weise, [frieren]).
+hazania(destruir_al_demonio_aura, auberst, [denken]).
+hazania(destruir_al_rey_demonio, ende, [frieren, himmel, heiter, eisen]).
+hazania(recuperar_al_gato_perdido, weise, [himmel, frieren]).
+hazania(destruir_a_schlat_el_omnisciente, ende, [el_heroe_del_sur]).
 
-recuerda(Persona, Hazania, AnioPedido):-
+recuerda(Persona, Hazania, AnioPedido) :-
     conocio(Persona, Hazania, FormaPresenciarlo, AnioDeRecuerdo),
     AnioPedido >= AnioDeRecuerdo,
-    recuerdaHasta(Persona, FormaPresenciarlo, AnioPedido, AnioDeRecuerdo).
+    estaViva(Persona, AnioPedido), % correccion 2b estaViva movido a recuerda. Ademas ya no se usa Persona en recuerdaHasta, ya que no es necesario para la logica de recordar.
+    recuerdaHasta(FormaPresenciarlo, AnioPedido, AnioDeRecuerdo).
 
-conocio(Persona, Hazania, Forma, Anio) :-
+conocio(Persona, Hazania, Forma, Anio) :- 
     recuerdo(Persona, Hazania, Forma, Anio).
 
-recuerdaHasta(Persona, presencio, AnioPedido, _):-
-    estaViva(Persona, AnioPedido).
+% correcciones al punto 2a
+recuerdaHasta(presencio, _, _).
 
-recuerdaHasta(Persona, cancion, AnioPedido, AnioDeRecuerdo):-
-    estaViva(Persona, AnioPedido),
+recuerdaHasta(cancion, AnioPedido, AnioDeRecuerdo) :-
     AnioHasta is AnioDeRecuerdo + 15,
     AnioPedido =< AnioHasta.
 
-recuerdaHasta(Persona, libro(Paginas), AnioPedido, AnioDeRecuerdo):-
-    estaViva(Persona, AnioPedido),
+recuerdaHasta(libro(Paginas), AnioPedido, AnioDeRecuerdo) :-
     AnioHasta is AnioDeRecuerdo + Paginas,
     AnioPedido =< AnioHasta.
 
-hazaniaCorroborada(Hazania):-
-    hazania(Hazania, UnLugar, Realizadores1),
-    not((hazania(Hazania, OtroLugar, Realizadores2), 
-            sonVersionesDistintas(UnLugar, Realizadores1, OtroLugar, Realizadores2)
-        )).
-
-sonVersionesDistintas(UnLugar, _, OtroLugar, _):-
-    UnLugar \= OtroLugar.
-
-sonVersionesDistintas(_, Realizadores1, _, Realizadores2):-
-    Realizadores1 \= Realizadores2.
+% correccion 2c hazaniaCorroborada usando forall
+hazaniaCorroborada(Hazania) :-
+    hazania(Hazania, LugarOriginal, RealizadoresOriginales),
+    forall(hazania(Hazania, OtroLugar, OtrosRealizadores), (LugarOriginal = OtroLugar, RealizadoresOriginales = OtrosRealizadores)).
 
 pasoAlOlvido(Hazania, Anio) :-
     hazania(Hazania, _, _), 
     not(recuerda(_, Hazania, Anio)).
 
-% PARTE N°3
-conmemoracion(weise, festividad(destruir_al_rey_demonio, 1340)).
-conmemoracion(auberst, estatua(bronce, el_equipo_de_heroes, destruir_al_rey_demonio, 1370)).
-conmemoracion(auberst, estatua(marmol, el_heroe_del_sur, destruir_a_schlat_el_omnisciente, 1340)).
+% correcion 3a sacamos el año del functor (hazaña).
+% conmemoracion(Pueblo, Hazaña, AnioInicio).
+conmemoracion(weise, festividad(destruir_al_rey_demonio), 1340).
+conmemoracion(auberst, estatua(bronce, el_equipo_de_heroes, destruir_al_rey_demonio), 1370).
+conmemoracion(auberst, estatua(marmol, el_heroe_del_sur, destruir_a_schlat_el_omnisciente), 1340).
 
 mantenimiento(el_equipo_de_heroes, 1400).
 mantenimiento(el_equipo_de_heroes, 1450).
@@ -92,19 +95,23 @@ mantenimiento(el_heroe_del_sur, 1410).
 
 conocio(Persona, Hazania, festividad, AnioConocio) :-
     habitante(Persona, _, Nacimiento, Pueblo),
-    conmemoracion(Pueblo, festividad(Hazania, AnioInicioFestividad)),
-    AnioConocio is max(Nacimiento, AnioInicioFestividad).
+    conmemoracion(Pueblo, festividad(Hazania), AnioInicio),
+    anioQueConocio(Nacimiento, AnioInicio, AnioConocio).
 
-conocio(Persona, Hazania, estatua(Material, NombreEstatua, Hazania, AnioInicio), AnioConocio) :-
+conocio(Persona, Hazania, estatua(Material, NombreEstatua, AnioInicio), AnioConocio) :-
     habitante(Persona, _, Nacimiento, Pueblo),
-    conmemoracion(Pueblo, estatua(Material, NombreEstatua, Hazania, AnioInicio)),
-    AnioConocio is max(Nacimiento, AnioInicio).
+    conmemoracion(Pueblo, estatua(Material, NombreEstatua, Hazania), AnioInicio),
+    anioQueConocio(Nacimiento, AnioInicio, AnioConocio).
 
-recuerdaHasta(Persona, festividad, AnioPedido, _) :-
-    estaViva(Persona, AnioPedido).
+% correcion 3b reemplazo del "is max()"
+anioQueConocio(Nacimiento, AnioInicio, AnioInicio) :-
+    Nacimiento =< AnioInicio.
+anioQueConocio(Nacimiento, AnioInicio, Nacimiento) :-
+    Nacimiento > AnioInicio.
 
-recuerdaHasta(Persona, estatua(Material, NombreEstatua, Hazania, AnioInicio), AnioPedido, _) :-
-    estaViva(Persona, AnioPedido),
+recuerdaHasta(festividad, _, _).
+
+recuerdaHasta(estatua(Material, NombreEstatua, AnioInicio), AnioPedido, _) :-
     estatuaEnBuenEstado(Material, NombreEstatua, AnioInicio, AnioPedido).
 
 vidaUtilMaterial(marmol, 30).
@@ -113,50 +120,44 @@ vidaUtilMaterial(bronce, 15).
 estatuaEnBuenEstado(Material, _, AnioInicio, AnioPedido) :-
     vidaUtilMaterial(Material, VidaMaxima),
     AnioPedido >= AnioInicio,
-    Diferencia is AnioPedido - AnioInicio,
-    Diferencia =< VidaMaxima.
+    AnioPedido - AnioInicio =< VidaMaxima.
 
 estatuaEnBuenEstado(Material, NombreEstatua, _, AnioPedido) :-
     vidaUtilMaterial(Material, VidaMaxima),
     mantenimiento(NombreEstatua, AnioMantenimiento),
     AnioPedido >= AnioMantenimiento,
-    Diferencia is AnioPedido - AnioMantenimiento,
-    Diferencia =< VidaMaxima.
+    AnioPedido - AnioMantenimiento =< VidaMaxima.
 
 
-% PARTE N°2
-% PUNTO 4
+% PARTE N°2 (Punto 4)
 
 recuerdaHazaniaPueblo(Pueblo, Hazania, AnioDado) :-
-    habitante(Persona,_,_,Pueblo),
+    habitante(Persona, _, _, Pueblo),
     recuerda(Persona, Hazania, AnioDado).
 
 cantidadDeHojasLeidas(Pueblo, AnioDado, CantidadTotalDePaginas) :-
-    findall(Paginas,(habitante(Persona,_,_,Pueblo),conocio(Persona, _, libro(Paginas), AnioDado)), TotalPaginas),
+    habitante(_, _, _, Pueblo), 
+    findall(Paginas, (habitante(Persona, _, _, Pueblo), conocio(Persona, _, libro(Paginas), AnioDado)), TotalPaginas),
     sum_list(TotalPaginas, CantidadTotalDePaginas).
     
-puebloMasLector(Pueblo, AnioDado):-
-    cantidadDeHojasLeidas(Pueblo, AnioDado, CantidadDeHojas),
-    not((
-        cantidadDeHojasLeidas(OtroPueblo, AnioDado, CantidadDeHojas2),
-        OtroPueblo \= Pueblo,
-        CantidadDeHojas2 > CantidadDeHojas
-    )).
+puebloMasLector(Pueblo, AnioDado) :-
+    cantidadDeHojasLeidas(Pueblo, AnioDado, MaxHojas),
+    forall(cantidadDeHojasLeidas(_, AnioDado, OtrasHojas), MaxHojas >= OtrasHojas). % cambio not por un forall
 
 puebloMusical(Pueblo, AnioDado) :-
-    cantidadHazaniasNoMusicales(Pueblo, AnioDado, CantidadTotalHazaniasNoMusicales),
-    cantidadHazaniasMusicales(Pueblo,AnioDado, CantidadTotalHazaniasMusicales),
-    CantidadTotalHazaniasMusicales > CantidadTotalHazaniasNoMusicales.
+    pueblo(Pueblo), % generador de pueblos
+    findall(Hazania, recuerdaHazaniaPueblo(Pueblo, Hazania, AnioDado), ListaHazanias),
+    list_to_set(ListaHazanias, HazaniasRecordadas), % list_to_set evita repetidos
+    findall(HazaniasMusicales, (member(HazaniasMusicales, HazaniasRecordadas), recordadaPorCancion(Pueblo, AnioDado, HazaniasMusicales)), HazaniasMusicales),
+    length(HazaniasRecordadas, CantidadHazaniasRecordadas), % musicales + no musicales = total de hazanias recordadas
+    length(HazaniasMusicales, CantidadHazaniasMusicales),
+    CantidadHazaniasMusicales > CantidadHazaniasRecordadas / 2.
 
-cantidadHazaniasNoMusicales(Pueblo, AnioDado, CantidadTotalHazaniasNoMusicales) :-
-    findall(Hazania, 
-        (habitante(Persona,_,_,Pueblo), recuerda(Persona, Hazania, AnioDado), conocio(Persona,Hazania, FormaDeConocimiento,_), FormaDeConocimiento \= cancion),
-        ListaHazaniasNoMusicales),
-    length(ListaHazaniasNoMusicales, CantidadTotalHazaniasNoMusicales).
-
-cantidadHazaniasMusicales(Pueblo, AnioDado, CantidadTotalHazaniasMusicales) :-
-    findall(Hazania, (habitante(Persona,_,_,Pueblo), recuerda(Persona, Hazania, AnioDado), conocio(Persona, Hazania, cancion,_)), ListaHazaniasMusicales),
-    length(ListaHazaniasMusicales, CantidadTotalHazaniasMusicales).
+recordadaPorCancion(Pueblo, AnioDado, Hazania) :- % auxiliar para verificar que si la persona recuerda la hazaña por cancion, esa cancion no este vencida.
+    habitante(Persona, _, _, Pueblo),
+    conocio(Persona, Hazania, cancion, AnioConocio),
+    estaViva(Persona, AnioDado),
+    recuerdaHasta(cancion, AnioDado, AnioConocio).
     
 esChismoso(Pueblo, AnioDado) :- 
     recuerdaHazaniaPueblo(Pueblo, _, AnioDado),
@@ -168,90 +169,81 @@ esChismoso(Pueblo, AnioDado) :-
 hazaniaImportante(Hazania, Pueblo, AnioDado) :-
     recuerdaHazaniaPueblo(Pueblo, Hazania, AnioDado),
     forall(
-        (habitante(Persona, _,_, Pueblo), estaViva(Persona, AnioDado)),
+        (habitante(Persona, _, _, Pueblo), estaViva(Persona, AnioDado)),
         recuerda(Persona, Hazania, AnioDado)   
     ).
 
 tiemposSinPrecedentes(Pueblo, AnioDado) :-
-    habitante(_, _ , _, Pueblo),
+    habitante(_, _, _, Pueblo),
     hazaniaImportante(_, Pueblo, AnioDado),
     forall(
         hazaniaImportante(Hazania, Pueblo, AnioDado),
-        (habitante(Persona,_,_,Pueblo),
-        recuerdo(Persona, Hazania, presencio, _))
+        (habitante(Persona, _, _, Pueblo), 
+        conocio(Persona, Hazania, presencio, _))
     ).
 
+% Punto 5
+
+heroe(Persona) :-
+    hazania(Hazania, _, Participantes),
+    member(Persona, Participantes), 
+    conocio(_, Hazania, _, _).      
+
+inspiro(Inspirador, Heroe) :-
+    heroe(Heroe),
+    conocio(Heroe, Hazania, _, _), 
+    hazania(Hazania, _, Participantes),
+    member(Inspirador, Participantes),
+    Inspirador \= Heroe.
+
+cadenaDeInspiracion(Inicial, Cadena) :-
+    heroe(Inicial),
+    inspiro(Inicial, Siguiente), 
+    armarCadena(Siguiente, [Inicial], CaminoInvertido),
+    reverse(CaminoInvertido, Cadena).
+
+armarCadena(Actual, Visitados, [Actual | Visitados]).
+armarCadena(Actual, Visitados, CadenaFinal) :-
+    inspiro(Actual, Siguiente),
+    not(member(Siguiente, Visitados)),
+    armarCadena(Siguiente, [Actual | Visitados], CadenaFinal).
+
+% TESTS
 
 :- begin_tests(tpIntegrador, []).
-    test(kanne_esta_viva_en_1370) :-
-        estaViva(kanne, 1370).
-    test(kanne_no_esta_viva_en_1300) :-
-        not(estaViva(kanne, 1300)).
-    test(kanne_no_esta_viva_en_1200) :-
-        not(estaViva(kanne, 1200)).
-    test(voll_esta_vivo_en_1550) :-
-        estaViva(voll, 1550).
-    test(voll_no_esta_vivo_en_1551) :-
-        not(estaViva(voll, 1551)).
-    test(serie_esta_viva_en_5000) :-
-        estaViva(serie, 5000).
-    test(lawine_no_recuerda_destruir_al_demonio_aura_en_1380) :-
-        not(recuerda(lawine, destruir_al_demonio_aura, 1380)).
-    test(lawine_recuerda_destruir_al_demonio_aura_en_1400) :-
-        recuerda(lawine, destruir_al_demonio_aura, 1400).
-    test(lawine_ya_no_recuerda_destruir_al_demonio_aura_1410) :-
-         not(recuerda(lawine, destruir_al_demonio_aura, 1410)).
-    test(voll_recuerda_destuir_al_demonio_aura_en_1450) :-
-        recuerda(voll, destruir_al_demonio_aura, 1450).
-    test(voll_no_recuerda_destuir_al_demonio_aura_en_1460) :-
-        not(recuerda(voll, destruir_al_demonio_aura, 1460)).
-    test(wirbel_recuerda_rescatar_a_la_hermana_en_1430) :-
-        recuerda(wirbel, rescatar_a_la_hermana, 1430).
-    test(wirbel_ya_no_recuerda_rescatar_a_la_hermana_en_1440) :-
-        not(recuerda(wirbel, rescatar_a_la_hermana, 1440)).
-    test(rescatar_a_la_hermana_es_corroborada) :-
-        hazaniaCorroborada(rescatar_a_la_hermana).
-    test(destruir_al_demonio_aura_no_es_corroborada) :-
-        not(hazaniaCorroborada(destruir_al_demonio_aura)).
-    test(destruir_al_demonio_aura_paso_al_olvido_en_1460) :-
-        pasoAlOlvido(destruir_al_demonio_aura, 1460).
-    test(destruir_al_demonio_aura_no_paso_al_olvido_en_1440) :-
-        not(pasoAlOlvido(destruir_al_demonio_aura, 1440)).
-    test(lawine_recuerda_rey_demonio_1400_por_estatua) :-
-        recuerda(lawine, destruir_al_rey_demonio, 1400).
-    test(lawine_no_recuerda_rey_demonio_1390_porque_estatua_rota) :-
-        not(recuerda(lawine, destruir_al_rey_demonio, 1390)).
-    test(fern_recuerda_rey_demonio_1400_por_festividad) :-
-        recuerda(fern, destruir_al_rey_demonio, 1400).
-    test(en_weise_se_recuerda_destruir_al_rey_demonio_en_1400) :-
-        recuerdaHazaniaPueblo(weise,destruir_al_rey_demonio,1400).
-    test(en_klares_se_recuerda_rescatar_a_la_hermana_de_wirbel_en_1395) :-
-        recuerdaHazaniaPueblo(klares,rescatar_a_la_hermana,1395).
-    test(en_klares_no_se_recuerda_destruir_al_rey_demonio_en_1395) :-
-        not(recuerdaHazaniaPueblo(klares,destruir_al_rey_demonio,1395)).
-    test(en_weise_se_leyeron_100_paginas_en_1335):-
-        cantidadDeHojasLeidas(weise, 1335, 100).
-    test(en_weise_se_leyeron_0_paginas_en_1336):-
-        cantidadDeHojasLeidas(weise, 1336, 0).
-    test(ende_es_el_pueblo_mas_lector_en_1440) :-
-        puebloMasLector(ende,1440).
-    test(auberst_es_musical_en_1395) :-
-        puebloMusical(auberst,1395).
-    test(weise_no_es_musical_en_1400) :-
-        not(puebloMusical(weise,1400)).
-    test(ende_es_chismoso_en_1420) :-
-        esChismoso(ende,1420).
-    test(weise_no_es_chismoso_en_1400) :-
-        not(esChismoso(weise,1400)).
-    test(destruir_al_rey_demonio_es_importante_para_weise_en_1400) :-
-        hazaniaImportante(destruir_al_rey_demonio, weise, 1400).
-    test(recuperar_al_gato_perdido_no_es_importante_para_weise_en_1400) :-
-        not(hazaniaImportante(recuperar_al_gato_perdido, weise, 1400)).
-    test(klares_vive_tiempos_sin_precedentes_en_1395) :-
-        tiemposSinPrecedentes(klares, 1395).
-    test(klares_vive_tiempos_sin_precedentes_en_1395) :-
-        tiemposSinPrecedentes(klares, 1395).
-    test(weise_no_vive_tiempos_sin_precedentes_en_1400) :-
-        not(tiemposSinPrecedentes(weise, 1400)).
-:- end_tests(tpIntegrador).
 
+    % --- Tests Parte 1 ---
+    test(persona_esta_viva_durante_su_expectativa_de_vida) :- estaViva(kanne, 1370).
+    test(persona_no_esta_viva_antes_de_su_nacimiento) :- not(estaViva(kanne, 1300)).
+    test(enano_esta_vivo_durante_su_expectativa_extendida) :- estaViva(voll, 1550).
+    test(enano_no_esta_vivo_al_superar_su_expectativa) :- not(estaViva(voll, 1551)).
+    test(elfo_esta_vivo_indefinidamente) :- estaViva(serie, 5000).
+    test(persona_no_recuerda_hazania_antes_de_conocerla) :- not(recuerda(lawine, destruir_al_demonio_aura, 1380)).
+    test(persona_recuerda_hazania_por_cancion_dentro_de_su_duracion) :- recuerda(lawine, destruir_al_demonio_aura, 1400).
+    test(persona_olvida_hazania_por_cancion_tras_vencer_duracion) :- not(recuerda(lawine, destruir_al_demonio_aura, 1410)).
+    test(hazania_unica_es_corroborada) :- hazaniaCorroborada(rescatar_a_la_hermana).
+    test(hazania_multiples_versiones_no_es_corroborada) :- not(hazaniaCorroborada(destruir_al_demonio_aura)).
+    test(hazania_sin_nadie_que_la_recuerde_pasa_al_olvido) :- pasoAlOlvido(destruir_al_demonio_aura, 1460).
+    test(persona_recuerda_hazania_por_estatua_mantenida) :- recuerda(lawine, destruir_al_rey_demonio, 1400).
+    test(persona_olvida_hazania_por_estatua_deteriorada) :- not(recuerda(lawine, destruir_al_rey_demonio, 1390)).
+
+    % --- Tests Punto 4 ---
+    test(pueblo_recuerda_hazania_si_al_menos_un_habitante_la_recuerda) :- recuerdaHazaniaPueblo(weise, destruir_al_rey_demonio, 1400).
+    test(pueblo_suma_correctamente_todas_las_paginas_leidas) :- cantidadDeHojasLeidas(weise, 1335, 100).
+    test(pueblo_con_mayor_cantidad_de_hojas_leidas_es_el_mas_lector) :- puebloMasLector(ende, 1400).
+    test(pueblo_musical_si_mayor_parte_de_hazanias_recordadas_son_canciones) :- puebloMusical(auberst, 1395).
+    test(pueblo_no_musical_si_canciones_no_superan_a_otros_medios) :- not(puebloMusical(weise, 1400)).
+    test(pueblo_chismoso_si_ninguna_hazania_recordada_esta_corroborada) :- esChismoso(ende, 1420).
+    test(hazania_importante_si_absolutamente_todo_el_pueblo_vivo_la_recuerda) :- hazaniaImportante(destruir_al_rey_demonio, weise, 1400).
+    test(tiempos_sin_precedentes_si_toda_hazania_importante_fue_presenciada) :- tiemposSinPrecedentes(klares, 1395).
+
+    % --- Tests Punto 5 ---
+    test(persona_participante_en_hazania_conocida_es_heroe) :- heroe(frieren).
+    test(persona_sin_participacion_en_hazania_conocida_no_es_heroe) :- not(heroe(wirbel)).
+    test(heroe_inspira_a_otro_si_este_conoce_su_hazania) :- inspiro(frieren, fern).
+    test(inspiracion_funciona_hacia_atras_en_el_tiempo) :- inspiro(stark, frieren).
+    test(cadena_de_inspiracion_valida_con_multiples_eslabones) :- cadenaDeInspiracion(himmel, [himmel, frieren, fern, denken]).
+    test(cadena_de_inspiracion_invalida_si_no_hay_relacion_de_inspiracion) :- not(cadenaDeInspiracion(denken, [denken, frieren])).
+    test(cadena_de_inspiracion_no_permite_ciclos_repetidos_del_mismo_heroe) :- not(cadenaDeInspiracion(frieren, [frieren, fern, frieren])).
+
+:- end_tests(tpIntegrador).
